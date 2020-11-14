2f10e372a78891d8d3f6e063d15c04285f5bc638
Yosevu Kilonzo <yosevuk@gmail.com>
2020-11-14 12:56:08 -0500

    solve(emacs-lisp): Hello World

---
+++ emacs-lisp/hello-world/hello-world.el
@@ -0,0 +1,13 @@
+;;; hello-world.el --- Hello World Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+
+(provide 'hello-world)
+
+(defun hello ()
+  "Return the string 'Hello, World!'."
+  "Hello, World!")
+;;; hello-world.el ends here
