84aca82d29473aeb12b56b9d327acbac62c6c4cc
Yosevu Kilonzo <yosevuk@gmail.com>
2020-11-15 20:55:21 -0500

    refactor(clojure): Reverse String (2)

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,8 +1,15 @@
 (ns reverse-string
   (:require  [clojure.string :as str]))
 
+;; Restrictions: reverse
+
+;; Version 1
+;; (defn reverse-string [s]
+;;   (str/join
+;;     (reduce #(cons %2 %1)
+;;             '()
+;;             (str/split s #""))))
+
+;; Version 2
 (defn reverse-string [s]
-  (str/join
-    (reduce #(cons %2 %1)
-            '()
-            (str/split s #""))))
+  (str/join (into () s)))
