722819095670c2e56bc2ef8c7589bee525ca465d
Yosevu Kilonzo <yosevuk@gmail.com>
2020-12-06 23:45:07 -0500

    refactor(haskell): Leap (2)

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,6 +1,4 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = if rem year 400 == 0
-                    then rem year 4 == 0 && rem year 100 == 0
-                    else rem year 4 == 0 && rem year 100 /= 0
+isLeapYear year = rem year 4 == 0 && (rem year 100 /= 0 || rem year 400 == 0)
