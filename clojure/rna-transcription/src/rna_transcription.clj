90d41547c753763cdadb70336ad56235568dcde9
Yosevu Kilonzo <yosevuk@gmail.com>
2020-12-16 22:13:11 -0500

    refactor(clojure): RNA Transcription (2)

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -3,11 +3,7 @@
 
 (def nucleotides {\G \C \C \G \T \A \A \U})
 
-(defn valid-strand
-  [strand]
-  (if (nucleotides strand)
-    (nucleotides strand)
-    (throw (AssertionError. "Invalid DNA strand."))))
-
 (defn to-rna [dna]
-  (str/join (map valid-strand dna)))
+  (let [rna (apply str (map #(nucleotides %) dna))]
+    (assert (every? #(contains? nucleotides %) dna))
+    rna))
