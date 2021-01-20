5b18a925b497e6d9cf0802a2ab69122232aceff3
Yosevu Kilonzo <yosevuk@gmail.com>
2021-01-19 21:52:53 -0500

    solve(javascript): Bob (2)

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,10 +1,12 @@
-const isQuestion = m => m.trim().slice(-1) === '?'
+const isQuestion = m => m.endsWith('?')
 const isUpperCase = m => m === m.toUpperCase() && m !== m.toLowerCase()
-const isEmpty = m => m.trim() === ''
+const isEmpty = m => m === ''
 
-export const hey = m =>
+const bob = m =>
   isQuestion(m) && !isUpperCase(m) ? "Sure."
 : isUpperCase(m) && !isQuestion(m) ? "Whoa, chill out!"
 : isQuestion(m) && isUpperCase(m)  ? "Calm down, I know what I'm doing!"
 : isEmpty(m)                       ? "Fine. Be that way!"
 : "Whatever."
+
+export const hey = m => bob(m.trim())
